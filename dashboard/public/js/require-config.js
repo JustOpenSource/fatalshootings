9fc73bda18906e4a950a0f624ad069846d2e729e
Troy Wolfe <troythewolfe@gmail.com>
2015-01-06 21:51:41 -0800

    testing out couchdb

--- dashboard/public/js/require-config.js
+++ dashboard/public/js/require-config.js
@@ -6,7 +6,9 @@ requirejs.config({
       "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery",
       "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min",
       "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
-      "data/fatalEncounters" : "app/data/fatalEncounters20141207"
+      "data/fatalEncounters" : "app/data/fatalEncounters20141207",
+      "data/processFatalData" : "app/data/processFatalData",
+       "utils/d3utils" : "app/utils/d3utils"
     }
 });
 
