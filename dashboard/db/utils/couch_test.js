c57d46e1fe86dd505b04d1d0545632d4a5e56800
Troy Wolfe <troythewolfe@gmail.com>
2015-01-20 21:38:11 -0800

    creating json dataset and install endpoint

--- dashboard/db/utils/couch_test.js
+++ dashboard/db/utils/couch_test.js
@@ -1,45 +1,30 @@
 var _ = require('underscore'),
 	nano = require('nano')('http://localhost:5984'),
-	data = require('../sample_data/pfdata.js'),
-	pfdb = nano.use('pf');
-/*
-//loop over each record to adjust data
-_.each(data, function(item, i){
-	delete item._dmair;
-	addRecords(item, i);
-});
+	data = require('../sample_data/pfcdata.js');
+	testvar = 'k',
+	pfdb = null;
 
 //add the record set to a database
-function addRecords(item, i){
-	pfdb.insert(item, 'fatality_' + i,  function(err, body, header) {
-		if (err) {
-			console.log('[pf.insert] ', err.message);
-			return;
-		}
-    });
+function addRecords(){
+	_.each(data.rows, function(item, i){
+		pfdb.insert(item, item.id, function(err, body, header) {
+			console.log('[pf' + testvar + '.insert] ' + item.id);
+				
+			if (err) {
+				console.log('[pf' + testvar + '.insert] ', err.message);
+				return;
+			}
+	    });
+	});
 }
-*/
-/*
-addRecords();
-*/
 
-/*
-nano.db.create('pf', function(err, body) {
-  if (!err) {
-    console.log('database alice created!');
-
-    pfdb = nano.use('pf');
-    addRecords();
-  } else {
-  	console.log(err);
-  }
+nano.db.create('pf' + testvar, function(err, body) {
+	if (!err) {
+		console.log(body);
+		//console.log('database pf' + testvar + ' created!');
+		//pfdb = nano.use('pf' + testvar);
+		//addRecords();
+	} else {
+		console.log(err);
+	}
 });
-*/
-
-/*
-var testObject = {
-	'foo' : 'bar'
-};
-
-module.exports = testObject;
-*/
