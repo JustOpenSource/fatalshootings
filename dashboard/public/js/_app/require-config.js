2f34304bb36d3d91a2a0d3505c7995320347554e
Troy Wolfe <troythewolfe@gmail.com>
2015-01-27 21:46:15 -0800

    Moving js logic to require js, wiring validator in to form preprocessing

---
+++ dashboard/public/js/_app/require-config.js
@@ -0,0 +1,15 @@
+requirejs.config({
+    "baseUrl": "/js",
+    "paths": {
+      "app": "/app",
+      "d3": "//cdnjs.cloudflare.com/ajax/libs/d3/3.4.13/d3",
+      "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery",
+      "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min",
+      "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+      "data/fatalEncounters" : "app/data/fatalEncounters20141207",
+      "data/processFatalData" : "app/data/processFatalData",
+       "utils/d3utils" : "app/utils/d3utils"
+    }
+});
+
+requirejs(["main"]);
--- dashboard/routes/gui.js
+++ dashboard/routes/gui.js
@@ -1,40 +1,69 @@
-var express = require('express');
-var router = express.Router();
-var c = require('../config/constants');
-var nano = require('nano')(c.nano);
-var db = nano.use(c.db_name);
-var _ = require('underscore');
-var e = require('../db/models/entry');
+var express = require('express'),
+    router = express.Router(),
+    c = require('../config/constants'),
+    nano = require('nano')(c.nano),
+    db = nano.use(c.db_name),
+    _ = require('underscore'),
+    e = require('../db/models/entry');
 
 router.route('/')
+    
     .get(function(req,res){
-        e.read({},function(output){
+        
+        e.read({}, function(output){
+            
             res.render('gui/list',{results: output,link_base:'/gui/'});
-        })
+        
+        });
+    
     })
+
     .post(function(req,res){
+        
         res.send('coming soon');
+    
     });
     
 router.route('/:id')
+
     .get(function(req,res){
+        
         if(_.isString(req.params.id)){
-            e.read({keys: [req.params.id]},function(body){
+            var readKeys = {
+                keys: [req.params.id]
+            };
+
+            e.read(readKeys, function(body){
+                
                 if(body.total_rows > 0){
-                    res.render('gui/single',{results: body.rows[0],link_base:'/gui/'});
-                }else{
+                    
+                    //business logic
+                    var process = require('./process-single');
+
+                    res.render('gui/single', {
+                        results: process(body.rows[0]),
+                        link_base:'/gui/'
+                    });
+
+                } else {
+                    
                     res.status(404).send('Not Found');
                 }
             });
-        }else{
+
+        } else {
+            
             res.status(404).send('Invalid ID');
         }
-    })
-    .put(function(req,res){
+
+    }).put(function(req,res){
+        
         res.send('coming soon!');
-    })
-    .delete(function(req,res){
+    
+    }).delete(function(req,res){
+        
         res.send('coming soon!');
+
     });
 
 module.exports = router;
