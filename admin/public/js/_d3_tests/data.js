a301bb3cc262ec631a0336d0502a560dc9558266
Troy Wolfe <troythewolfe@gmail.com>
2015-02-19 22:32:23 -0800

    more refactor to split it into three apps, wiring in pagination html template component

--- admin/public/js/_d3_tests/data.js
+++
@@ -1,3 +0,0 @@
-define("fatalEncounters", [], function(){
-	return {'foo' : 'bar'}
-});
--- explore/public/js/main/require-config.js
+++ explore/public/js/main/require-config.js
@@ -1,6 +1,7 @@
 requirejs.config({
     "baseUrl": "/js",
     "paths": {
+    	"d3": "//cdnjs.cloudflare.com/ajax/libs/d3/3.4.13/d3",
       	"jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery-min",
       	"underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min",
       	"backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min"
