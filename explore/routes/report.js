22dc431e43e236d930719ee868cdd4a25bc33469
Troy Wolfe <troythewolfe@gmail.com>
2015-05-02 18:45:12 -0700

    Stubbing out report form.

---
+++ explore/routes/report.js
@@ -0,0 +1,31 @@
+var c = require(__base + '../shared-config/constants'),
+    log = c.getLog('explore/routes/report');
+    
+    //router
+    router = require('express').Router(),
+    
+    renderView = require(__base + '../shared-utils/render-view');
+
+
+// url/list/
+router.route('/')
+.get(function(req, res){
+
+    var page_title = 'Report A Death';
+
+    renderView(req, res, 'report', {
+    	//report params
+    }, {
+    	//page params
+        title: page_title,
+        css: ['reportADeathForm']
+    });
+
+})
+
+.post(function(req, res){
+
+    log('trace', 'post date', req.body);
+});
+
+module.exports = router;
