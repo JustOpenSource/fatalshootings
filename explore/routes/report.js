998f33b071baa5fc6fda8d95d4b522aeffb61894
Troy Wolfe <troythewolfe@gmail.com>
2015-06-27 22:34:47 -0700

    Added basic validation

--- explore/routes/report.js
+++ explore/routes/report.js
@@ -4,11 +4,103 @@ var c = require(__base + '../shared-config/constants'),
     //router
     router = require('express').Router(),
     
-    renderView = require(__base + '../shared-utils/render-view');
+    renderView = require(__base + '../shared-utils/render-view'),
+    validate = require(__base + '../shared-utils/schema-validator');
 
 
-function cleanPost(report){
-    return report;
+/*
+reporter_name
+reporter_email
+deceased_name
+deceased_age
+deceased_sex 
+deceased_race 
+deceased_raceOther
+deceased_mentalIllness
+circumstances_date
+circumstances_city
+circumstances_state
+circumstances_zip
+circumstances_cause
+circumstances_causeOther
+circumstances_disposition
+circumstances_dispositionOther
+circumstances_notes
+officer_firstName
+officer_middleName
+officer_lastName
+officer_agency
+source_url
+*/
+
+function formatEntry(entry){
+
+    function stringToBool(bool){
+        return bool.toLowerCase() == 'true' ? true : false;
+    }
+
+    function formatDate(date){
+        data = date.split('/');
+
+        return parseInt(date[2] + date[1] + date[0]);
+    }
+    
+    var formattedEntry = {
+        "submitted_by" : entry.reporter_name,
+        "published" : false,
+        "subject" : {
+            "sex": entry.deceased_sex,
+            "cis" : stringToBool(entry.deceased_cis),
+            "name" : entry.deceased_name,
+            "race" : entry.deceased_race,
+            "mental_illness" : entry.deceased_mentalIllness
+        },
+
+        "death" : {
+
+            "cause" : entry.circumstances_cause,
+            "cause_notes" : entry.circumstances_causeOther,
+            "responsible_agency" : entry.officer_agency,
+            "description" : entry.circumstances_notes,
+            "disposition" : entry.circumstances_disposition,
+            "disposition_notes" : entry.circumstances_dispositionOther,
+            "source_url" : entry.source_url
+        },
+
+        "encounter" : {
+
+            "location" : {
+
+                "coordinates" : {
+                    "latitude" : null,
+                    "longitude" : null
+                },
+
+                "country" : {
+
+                    "USA": {
+
+                        "city": entry.circumstances_city,
+
+                        "state": entry.circumstances_state,
+
+                        "zipcode": entry.circumstances_zip
+                    }
+                }
+            }
+        }
+    }
+
+    //only add these if they exist
+    if(entry.deceased_age !== "unknown") {
+        formattedEntry.subject.aga = parseInt(entry.deceased_age);
+    }
+
+    if(entry.circumstances_date !== ""){
+        formattedEntry.encounter.date = formatDate(entry.circumstances_date);
+    }
+
+    return formattedEntry;
 }
 
 // url/list/
@@ -30,14 +122,24 @@ router.route('/')
 
     log('trace', 'post data', req.body);
 
-    var report = cleanPost(req.body);
+    var report = formatEntry(req.body);
 
+    log('trace', report);
+
+    var validatedReport = validate(report);
+
+    log('trace', validatedReport);
+
+    res.redirect('/report');
+
+    /*
     report.pending = true;
 
     req._db.fatalities.insert(req.body, function(err, body){
 
         log('trace', 'new entry added');
     });
+    */
 });
 
 module.exports = router;
