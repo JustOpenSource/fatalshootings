8de14caae1602d8188d786d7720cb55b29f4163e
Troy Wolfe <troythewolfe@gmail.com>
2015-08-11 23:40:23 -0700

    Merging report and detail page

--- explore/routes/details.js
+++ explore/routes/details.js
@@ -1,11 +1,90 @@
 var c = require(__base + '../shared-config/constants'),
-    log = c.getLog('explore/routes/details');
+    log = c.getLog('explore/routes/details'),
+
+    _ = require('underscore'),
     
     //router
     router = require('express').Router(),
     
     renderView = require(__base + '../shared-utils/render-view');
 
+
+function formatEntryForDatabase(entry){
+    
+    var formattedEntry = {
+        "submitted_by_name" : entry.reporter_name,
+        "submitted_by_email" : entry.reporter_email,
+        "published" : false,
+
+        "subject" : {
+            "sex": entry.subject_sex,
+            "orientation": entry.subject_orientation,
+            "cisgendered" : entry.subject_cis,
+            "name" : entry.subject_name,
+            "race" : entry.subject_race,
+            "race_other" : entry.subject_race_other,
+            "mental_illness" : entry.subject_mental_illness,
+            "mental_illness_other" : entry.subject_mental_illness_other
+        },
+
+        "death" : {
+            "cause" : entry.death_cause,
+            "cause_notes" : entry.death_causeOther,
+            "responsible_agency" : entry.officer_agency,
+            "description" : entry.death_notes,
+            "disposition" : entry.death_disposition,
+            "disposition_other" : entry.death_dispositionOther,
+            "disposition_notes" : entry.death_notes,
+        },
+
+        "location" : {
+            "coordinates" : {
+                "latitude" : null,
+                "longitude" : null
+            },
+
+            "country" : entry.location_country,
+            "address_1": entry.location_address_line_1,
+            "address_2": entry.location_address_line_2,
+            "city": entry.location_city,
+            "state": entry.location_state,
+            "zipcode": entry.location_zip
+        },
+
+        "sources" : entry.sources
+    }
+
+    //only add these if they exist
+    if(entry.subject_age !== "unknown") {
+        formattedEntry.subject.age = parseInt(entry.subject_age);
+    }
+
+    if(entry.death_date !== ""){
+        formattedEntry.death.date = entry.death_date;
+    }
+
+    return formattedEntry;
+}
+
+function testForRequiredValues(report){
+    var missing = [];
+
+    var requiredValues = [
+        [report.subject.name, 'subject_name'],
+        [report.location.country, 'location_country'],
+        [report.location.state, 'location_state'],
+        [report.death.date, 'death_date'],
+        [report.sources, 'sources']
+    ];
+
+    _.each(requiredValues, function(value){
+        if(!value[0]){
+            missing.push(value[1]);
+        }
+    });
+
+    return missing;
+}
 
 // url/details/
 router.route('/:id')
@@ -14,14 +93,50 @@ router.route('/:id')
     var page_title = 'Details';
 
     renderView(req, res, 'details', {
-    
-    	"id" : req.params.id
-    
+        
+    	"id" : req.params.id,
+        "edit" : req.query.edit,
+        "missing" : req.query.missing,
+        "success" : req.query.success,
+        "missing_values" : req.query.missing_values,
+        "details" : req.query.details ? JSON.parse(req.query.details) : null
+
     }, {
 
         title: page_title,
         css: ['details']
     });
+})
+
+.post(function(req, res){
+
+    var report = formatEntryForDatabase(req.body);
+    var required = testForRequiredValues(report);
+    var missing = required.length > 0 ? true : false;
+
+    if(missing){
+
+        res.redirect('/details/new?missing=true&missing_values=' + required.join(',') + '&details=' + JSON.stringify(report));
+
+    } else {
+
+        report = {
+            'value' : report
+        }
+        
+        report.pending = true;
+
+        req._db.fatalities.insert(report, function(err, body){
+
+            if(err){
+                res.redirect('/details/new?error=true');
+            } else {
+                res.redirect('/details/new?success=true');
+            }
+
+            log('trace', 'new entry added');
+        });
+    }
 });
 
 module.exports = router;
--- shared-views/details.js
+++ shared-views/details.js
@@ -19,72 +19,387 @@ function getDetails(id, cb) {
 	/**/
 }
 
+function generateOptions(options, selected){
+    var optionsObj = [];
+
+    _.each(options, function(curr){
+        optionsObj.push({
+            'selected' : selected == curr ? true : false,
+            'value' : curr,
+            'text' : curr
+        });
+    });
+
+    return optionsObj;
+}
+
+/*
+TODO: Generate option values from schema
+*/
+function ageOptions(selected){
+    var ages = [],
+        upperLimit = 120,
+        curr = 1;
+
+    selected = parseInt(selected);
+
+    while(curr <= upperLimit){
+        
+        ages.push({
+            'selected' : selected === curr ? true : false,
+            'value' : curr,
+            'text' : curr
+        });
+
+        curr++;
+    }
+
+    return ages;
+}
+
+function sexOptions(selected){
+
+    console.log('SEX OPTIONS -----------');
+    console.log(selected);
+
+    return generateOptions([
+      'Male', 
+      'Female', 
+      'Intersex'], selected);
+}
+
+function orientationOptions(selected){
+
+    return generateOptions([
+      'Heterosexual', 
+      'Homosexual', 
+      'Bisexual', 
+      'Other'], selected);
+}
+
+
+/*
+TODO: Should this be a true/false checkbox?
+*/
+function cisgenderedOptions(selected){
+
+    return generateOptions([
+      'cisgender', 
+      'transgender'], selected);
+}
+
+function raceOptions(selected){
+
+    return generateOptions([
+      'White / European', 
+      'Asain Indian',
+      'Middle Eastern',
+      'Black / African American',
+      'Native American or Alaska Native',
+      'Hispanic or Latino',
+      'Jewish',
+      'Asian',
+      'Pacific Islander',
+      'Indigenous Australian',
+      'Other'], selected);
+}
+
+function mentalIllnessOptions(selected){
+
+    return generateOptions([
+      'Schizophrenia',
+      'Manic Depression',
+      'Depression',
+      'Anger Problems',
+      'Anxiety',
+      'Sociopathy'], selected);
+}
+
+function causeOfDeathOptions(selected){
+
+    return generateOptions([
+      'Gunshot',
+        'Assault',
+        'Vehicle',
+        '"Non Lethal" Weapon',
+        'Unknown',
+        'Other'], selected);
+}
+
+function dispositionOptions(selected){
+
+    return generateOptions([
+      'Justified',
+      'Homocide',
+      'Other'], selected);
+}
+
+function countryOptions(selected){
+
+    return generateOptions([
+      'United States',
+      'Mexico'], selected);
+}
+
 function formatDetails(details){
 
-  	function pushValues(values){
-  		var list = [];
+    function processSectionInputs(section){
 
-  		_.each(values, function(value){
-  			list.push({
-  				'key' : value[0],
-  				'value' : value[1]
-  			})
-  		});
+      _.each(section, function(input, i){
 
-  		return list;
-  	}
+        input.status = {};
+
+        if(details.missing_values && details.missing_values.indexOf(input.name) > -1){
+          input.status.missing = true;
+        }
+
+        if(input.required){
+          input.status.required = true;
+        }
+
+        if(typeof input.options === 'function'){
+          
+          input.options = input.options(input.value);
+        }
+      });
+
+      return section;
+    }
+
+    if(!details){
+      var details = {
+        'value' : {
+          'subject' : {},
+          'death' : {
+            event: {}
+          },
+          'location' : {}
+        }
+      }
+    }
 
   	var subject = [
-  		['name', details.value.subject.name, 'subject-name'],
-  		['age', details.value.subject.age],
-  		['sex', details.value.subject.sex],
-  		['race', details.value.subject.race],
-  		['mentall illness', details.value.subject.mental_illness, 'mental-illness']
+  		{
+        'label' : 'Name', 
+        'value' : details.value.subject.name, 
+        'name' : 'subject_name',
+        'input-text' : true,
+        'required' : true
+      },
+      {
+        'label' : 'Age', 
+        'value' : details.value.subject.age, 
+        'name' : 'subject_age',
+        'input-select' : true,
+        'options' : ageOptions
+      },
+      {
+        'label' : 'Sex', 
+        'value' : details.value.subject.sex, 
+        'name' : 'subject_sex',
+        'input-select' : true,
+        'options' : sexOptions
+      },
+      {
+        'label' : 'Orientation', 
+        'value' : details.value.subject.orientation, 
+        'name' : 'subject_orientation',
+        'input-select' : true,
+        'options' : orientationOptions
+      },
+      {
+        'label' : 'Transgender',
+        'value' : details.value.subject.transgender,
+        'name' : 'subject_transgender',
+        'input-checkbox' : true
+      },
+      {
+        'label' : 'Race', 
+        'value' : details.value.subject.race, 
+        'name' : 'subject_race',
+        'input-select' : true,
+        'options' : raceOptions
+      },
+      {
+        'label' : 'Mentall Illness', 
+        'value' : details.value.subject.mental_illness, 
+        'name' : 'subject_mental_illness',
+        'input-select' : true,
+        'options' : mentalIllnessOptions
+      }
   	];
 
-  	var death = [
-  		['date', details.value.death.event.date],
-  		['cause', details.value.death.cause],
-  		['notes', details.value.location.death],
-  		['agency', details.value.death.responsible_agency],
-  		['description', details.value.death.description],
-  		['disposition', details.value.death.disposition]
-  	];
+    var death = [
+      {
+        'label' : 'Date', 
+        'value' : details.value.death.date, 
+        'name' : 'death_date',
+        'input-date' : true,
+        'required' : true
+      },
+      {
+        'label' : 'cause', 
+        'value' : details.value.death.cause, 
+        'name' : 'death_cause',
+        'input-select' : true,
+        'options' : causeOfDeathOptions
+      },
+      {
+        'label' : 'Cause Notes', 
+        'value' : details.value.death.notes, 
+        'name' : 'death_cause_notes',
+        'input-textarea' : true
+      },
+      {
+        'label' : 'Responsible Agency', 
+        'value' : details.value.death.responsible_agency, 
+        'name' : 'death_responsible_agency',
+        'input-text' : true
+      },
+      {
+        'label' : 'Description', 
+        'value' : details.value.death.description, 
+        'name' : 'death_description',
+        'input-textarea' : true
+      },
+      {
+        'label' : 'Disposition', 
+        'value' : details.value.death.disposition, 
+        'name' : 'death_disposition',
+        'input-select' : true,
+        'options' : dispositionOptions
+      }
+    ];
 
-  	var location = [
-  		['country', details.value.location.country || 'us'],
-  		['city', details.value.location.city],
-  		['state', details.value.location.state],
-  		['county', details.value.location.county],
-  		['zip', details.value.location.zip]
-  	];
+    var location = [
+      {
+        'label' : 'Address Line 1', 
+        'value' : details.value.location.address_line_1, 
+        'name' : 'location_address_line_1',
+        'input-select' : true,
+        'options' : countryOptions
+      },
+      {
+        'label' : 'Address Line 2', 
+        'value' : details.value.location.address_line_2,
+        'name' : 'location_address_line_2',
+        'input-select' : true,
+        'options' : countryOptions
+      },
+      {
+        'label' : 'Country', 
+        'value' : details.value.location.country || 'us', 
+        'name' : 'location_country',
+        'input-select' : true,
+        'options' : countryOptions,
+        'required' : true
+      },
+      {
+        'label' : 'City', 
+        'value' : details.value.location.city, 
+        'name' : 'location_city',
+        'input-text' : true
+      },
+      {
+        'label' : 'State', 
+        'value' : details.value.location.state, 
+        'name' : 'location_state',
+        'input-text' : true,
+        'required' : true
+      },
+      {
+        'label' : 'County', 
+        'value' : details.value.location.county, 
+        'name' : 'location_county',
+        'input-text' : true
+      },
+      {
+        'label' : 'Postal Code', 
+        'value' : details.value.location.zip, 
+        'name' : 'location_zip',
+        'input-text' : true
+      }
+    ];
 
-  	return [{
+    var sources = [
+      {
+        'label' : 'Sources',
+        'value' : details.value.sources, 
+        'name' : 'sources',
+        'input-textarea' : true,
+        'notes' : 'Use "enter" key to make a new line for each source.',
+        'required' : true
+      }
+    ];
+
+    if(details.edit){
+      sources.push({
+        'label' : 'Edit Notes',
+        'value' : details.value.edit_notes, 
+        'name' : 'edit_notes',
+        'input-textarea' : true,
+        'notes' : 'Please include any comments about your edits.'
+      });
+    }
+
+    var sections = [{
   		'sectionTitle' : 'Subject',
-  		'values' :  pushValues(subject)
+  		'values' :  processSectionInputs(subject)
   	}, {
-  		'sectionTitle' : 'Death',
-  		'values' :  pushValues(death)
-  	}, {
-  		'sectionTitle' : 'Location',
-  		'values' :  pushValues(location)
-  	}]
+      'sectionTitle' : 'Death',
+      'values' :  processSectionInputs(death)
+    }, {
+      'sectionTitle' : 'Location',
+      'values' :  processSectionInputs(location)
+    }, {
+      'sectionTitle' : 'Additional Info',
+      'values' :  processSectionInputs(sources)
+    }];
+
+    return sections;
 }
 
 module.exports = function(d, cb) {
 
-	getDetails(d.id, function(err, body){
-		if(err){
-			log('error', 'get details error', err);
+  if(d.id === 'new'){
 
-			//cb(err)
-		}
+    var details = null;
 
-		log('trace', 'get details', body);
+    if(d.missing){
+      details = {
+        value : d.details,
+        missing_values : d.missing_values
+      }
+    }
 
-		cb(null, {
-			'id' : d.id,
-			'sections' : formatDetails(body)
-		});
-	});
+    cb(null, {
+      'edit' : true,
+      'new' : true,
+      'id' : d.id,
+      'sections' : formatDetails(details),
+      'success' : d.success
+    });
+
+  } else {
+
+  	getDetails(d.id, function(err, body){
+  		if(err){
+  			log('error', 'get details error', err);
+
+  			//cb(err)
+  		}
+
+  		log('trace', 'get details', body);
+
+      if(d.edit){
+        body.edit = true;
+      }
+
+  		cb(null, {
+        'edit' : d.edit || false,
+  			'id' : d.id,
+  			'sections' : formatDetails(body)
+  		});
+  	});
+  }
 }
