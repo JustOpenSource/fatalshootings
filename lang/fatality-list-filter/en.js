07235a1572c4adee24270a0ca3bbb086a366aecb
troythewolfe <troythewolfe@gmail.com>
2015-08-28 23:27:25 -0700

    add lang file

---
+++ lang/fatality-list-filter/en.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"input_default_name" : "Name",
+	"select_label_prefix" : "All",
+	"select_label_sex" : "Sex",
+	"select_label_race" : "Race",
+	"select_label_cause" : "Cause",
+	"filter_button" : "Filter"
+}
