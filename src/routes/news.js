3f378334e6b79e144523a645a42cc9519ffef2e3
troythewolfe <troythewolfe@gmail.com>
2016-08-14 09:50:21 -0700

    stubbing out pages

---
+++ src/routes/news.js
@@ -0,0 +1,28 @@
+var __base = __base || '../';
+var c = require(__base + 'constants');
+var log = require(__base + 'utils/log')('routes/contact');
+var router = require('express').Router();
+var renderView = require(__base + 'utils/render-view');
+
+
+// url/list/
+router.route('/')
+.get(function(req, res){
+
+    var page_title = 'News & Updates';
+
+    renderView(req, res, 'news', {
+        "msg" : req.query.msg
+    }, {
+
+        title: page_title
+    });
+})
+
+.post(function(req, res){
+
+    log('trace', 'post date', req.body);
+
+});
+
+module.exports = router;
